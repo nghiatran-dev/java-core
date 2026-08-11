921203aa8041cb8464a9c6adf10c4db8ee8e8884
Tran Trong Nghia <trantrongnghia.vn@smilegate.com>
2026-08-11 16:10:31 +0700

    chapter3#22

--- src/chapter2/HelloWorld.java
+++ src/chapter2/HelloWorld.java
@@ -2,7 +2,15 @@ package chapter2;
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World Victor!");
+		System.out.println("Hello World!");
+		String myName = "Victor";
+		String myJob = "Developer";
+		String myAge = "33A";
+		int[] scores = { 90, 85, 80 };
+		System.out.println("My name: " + myName);
+		System.out.println("My job: " + myJob);
+		System.out.println("My age: " + Integer.valueOf(myAge));
+		System.out.println("My scores: " + scores[0] + ", " + scores[1] + ", " + scores[2]);
 	}
 
 }
